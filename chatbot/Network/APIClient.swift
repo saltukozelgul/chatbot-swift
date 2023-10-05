6a851e4ce60797030a42b826b16626c46d2cf3ab
saltukozelgul <saltukozelgul@gmail.com>
2023-10-05 21:22:03 +0300

    basic project folder layout created

---
+++ chatbot/Network/APIClient.swift
@@ -0,0 +1,8 @@
+//
+//  APIClient.swift
+//  chatbot
+//
+//  Created by Saltuk Bugra OZELGUL on 5.10.2023.
+//
+
+import Foundation
---
+++ chatbot/Views/ChatMessageCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatMessageCell.swift
+//  chatbot
+//
+//  Created by Saltuk Bugra OZELGUL on 5.10.2023.
+//
+
+import UIKit
+
+class ChatMessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
