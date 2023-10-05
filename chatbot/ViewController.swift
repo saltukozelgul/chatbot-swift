de16532f715576d759bd323106c8b8b45019406e
saltukozelgul <saltukozelgul@gmail.com>
2023-10-05 21:13:28 +0300

    inital commit

---
+++ chatbot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chatbot
+//
+//  Created by Saltuk Bugra OZELGUL on 5.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
